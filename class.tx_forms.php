979a81c784ec75ce0cbf02849d8b0474b20919d8
thesquire <thesquire@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-07-27 16:32:07 +0000

    comment code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/forms/trunk@3536 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_forms.php
+++ class.tx_forms.php
@@ -9,7 +9,7 @@
 //* 
 //****************************************************************
 
-// TODO: file form
+// TODO: add some kind of error if name isn't given in the form building methods
 class tx_forms {
 	
 	var $fields;		// array of all the field objects
@@ -35,9 +35,11 @@ class tx_forms {
 	 * 	
 	 */
 	function input($attr) {
-		
+
+		// get the name from the attribute array	
 		$name = $attr['name'];
-		
+
+		// add new field to global array in form object		
 		$this->fields[$name] = new formText($attr);
 	}
 
@@ -47,9 +49,11 @@ class tx_forms {
 	 * @param array $attr associative array that defines the attributes inside the input tag; it's not validated in any way.
 	 */
 	function textarea($attr, $value) {
-		
+
+		// get the name from the attribute array		
 		$name = $attr['name'];
-		
+
+		// add new field to global array in form object		
 		$this->fields[$name] = new formTextarea($attr, $value);
 	}
 
@@ -62,11 +66,12 @@ class tx_forms {
 	 * @param array $options associative array that defines the options as 'value'=>'Text' pairs
 	 */
 	function select($selectAttr, $optionsAttr, $selected, $options) {
-		
+
+		// get the name from the attribute array		
 		$name = $selectAttr['name'];
-		
+
+		// add new field to global array in form object	
 		$this->fields[$name] = new formSelect($selectAttr, $optionsAttr, $selected, $options);
-		
 	}
 	
 	/**
@@ -78,10 +83,12 @@ class tx_forms {
 	 * @param array $options associative array that defines the options as 'value'=>'Text' pairs
 	 */
 	function selectList($selectAttr, $optionsAttr, $selected, $options) {
-		
+
+		// get the name from the attribute array		
 		$selectAttr['multiple'] = 'multiple';
+
+		// add new field to global array in form object
 		$this->select($selectAttr, $optionsAttr, $selected, $options);
-	
 	}
 	
 
@@ -92,11 +99,12 @@ class tx_forms {
 	 * @param boolean $checked whether or not the checkbox is checked
 	 */
 	function checkbox($attr, $checked = false) {
-		
+	
+		// get the name from the attribute array
 		$name = $attr['name'];
 		
+		// add new field to global array in form object
 		$this->fields[$name] = new formCheckbox($attr, $checked);
-	
 	}
 	
 	/**
@@ -105,14 +113,15 @@ class tx_forms {
 	 * @param array $attr holds all attributes for each radio tag
 	 * @param string $checked value of the radio button that is to be selected
 	 * @param array $options array of options, or values.
-	 * TODO: see on top, need to figure something out with labels.
+	 * TODO: need to figure something out with labels.
 	 */
 	function radio($attr, $checked, $options) {
-		
+
+		// get the name from the attribute array	
 		$name = $attr['name'];
 		
+		// add new field to global array in form object
 		$this->fields[$name] = new formRadio($attr, $checked, $options);
-
 	}
 	
 	/**
@@ -122,9 +131,11 @@ class tx_forms {
 	 * 	
 	 */
 	function password($attr) {
-		
+
+		// get the name from the attribute array	
 		$name = $attr['name'];
-		
+
+		// add new field to global array in form object		
 		$this->fields[$name] = new formPassword($attr);
 	}
 	
@@ -136,8 +147,10 @@ class tx_forms {
 	 */
 	function hidden($attr) {
 
+		// get the name from the attribute array
 		$name = $attr['name'];
 		
+		// add new field to global array in form object
 		$this->fields[$name] = new formHidden($attr);
 	}
 	
@@ -149,24 +162,51 @@ class tx_forms {
 	 */
 	function file($attr) {
 		
+		// get the name from the attribute array
 		$name = $attr['name'];
 		
+		// add new field to global array in form object
 		$this->fields[$name] = new formFile($attr);
 	}
 	
+//****************************************
+//******** rendering methods  ************
+//****************************************
 
+	/**
+	 * @name render
+	 * @abstract renders the complete form
+	 * @return string complete form as html
+	 * 
+	 * TODO: add <form> stuff
+	 */
 	function render() {
 		
+		// initialize output
 		$output = null;
+		
+		// loop through all the fields and call render
+		// method on each and add return value to output
 		foreach($this->fields as $field) {
 			$output .= $field->render();
 		}
+		
 		return $output;
 	}
 	
+	/**
+	 * @name renderSingle
+	 * @abstract renders a single field of the form
+	 * @param string $name name of the field to be rendered
+	 * @return string field as html
+	 * 
+	 * TODO: add form rendering
+	 */	
 	function renderSingle($name) {
+		
+		// get field object by name
 		$field = $this->fields[$name];
-		//print_r($field);
+
 		return $field->render();
 	}
 }
